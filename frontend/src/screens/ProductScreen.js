cbe6a8222e5670183179588b913da84a88bce895
unknown <mayur.dere7@gmail.com>
2020-10-20 01:36:05 +0530

    Frontend restored and tested

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function ProductScreen(props) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      //
+    };
+  }, []);
+
+  return (
+    <div>ProductScreen</div>
+  );
+}
+export default ProductScreen;
