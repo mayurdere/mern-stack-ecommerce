2f50c60dcf879bf9ef32918bfbb918a85ba24d34
unknown <mayur.dere7@gmail.com>
2020-10-19 06:38:21 +0530

    First Commit

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default  {
+    products: [
+        {   
+            _id: '1',
+            name: 'Dove Soap',
+            category: 'Soap',
+            image: '/images/d1.png',
+            price: 60,
+            brand: 'Hindustan Uniliver',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Lux Soap',
+            category: 'Soap',
+            image: '/images/d1.png',
+            price: 20,
+            brand: 'Hindustan Uniliver',
+            rating: 4.1,
+            numReviews: 10,
+            countInStock: 5
+        },
+        {
+            _id: '3',
+            name: 'Cinthol',
+            category: 'Body wash',
+            image: '/images/d1.png',
+            price: 160,
+            brand: 'Hindustan Uniliver',
+            rating: 5,
+            numReviews: 10,
+            countInStock: 4
+        },
+        {
+            _id: '4',
+            name: 'Cinthol',
+            category: 'Body wash',
+            image: '/images/d1.png',
+            price: 160,
+            brand: 'Hindustan Uniliver',
+            rating: 5,
+            numReviews: 10,
+            countInStock: 2
+        }
+    ]
+}
